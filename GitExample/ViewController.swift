8cc4c97f6ddf60f0e61e16bc8998635568d099c5
{excuilax} <Benja@macbook-pro-de-admin.local>
2016-07-18 20:00:43 -0500

    Extra comment

--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // Extra comment
+        // 2
     }
 
     override func didReceiveMemoryWarning() {
